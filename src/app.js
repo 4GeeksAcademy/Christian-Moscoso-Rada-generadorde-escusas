3a045e43cf4d3a6b33d8cc8e95f2718666edd7cc
chriss-camr <chriss02.camr@gmail.com>
2024-11-25 12:06:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let subjects = ["My dog", "My cat", "My Neighbor", "A Stranger"];
+  let actions = ["ate", "stole", "hid", "destroyed"];
+  let objects = ["my homework", "my keys", "my lunch", "my proyect"];
+  let times = ["yesterday", "this morning", "last night", "a few minutes ago"];
+
+  const generateExcuse = () => {
+    let randomSubject = Math.floor(Math.random() * subjects.length);
+    let randomAction = Math.floor(Math.random() * actions.length);
+    let randomObject = Math.floor(Math.random() * objects.length);
+    let randomTime = Math.floor(Math.random() * times.length);
+
+    let excuse =
+      subjects[randomSubject] +
+      " " +
+      actions[randomAction] +
+      " " +
+      objects[randomObject] +
+      " " +
+      times[randomTime];
+    document.querySelector("#excuse").innerHTML = excuse;
+  };
+  generateExcuse();
+  document
+    .querySelector("#generate-btn")
+    .addEventListener("click", generateExcuse);
 };
